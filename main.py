5303cc29dcc56d0f0f78841e8285dfc1d23b6dc7
Pavel Samoshin <pasamoshin@gmail.com>
2022-10-20 22:48:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import hashlib
+import os.path
+
+
+def get_md5(path=str):
+    with open(path, 'rb') as file:
+        data = file.read()
+        md5 = hashlib.md5(data).hexdigest()
+    return md5
+
+
+def get_size(path=str):
+    size = os.path.getsize(path)
+    formatted_size = f'{size:,}'.replace(',', ' ')
+    return formatted_size
+
+
+def creat_res_file(path, md5, size):
+    path = os.path.split(path)
+    path_res_file = os.path.join(path[0], f'{path[1]} - md5 and size.txt')
+    with open(path_res_file, 'w') as file:
+        file.write(f'MD5 - {md5}\nSize - {size} bytes')
+
+
+if __name__ == '__main__':
+    path = input('Введите путь: ').replace('"', '')
+    md5 = get_md5(path)
+    size = get_size(path)
+    creat_res_file(path, md5, size)
